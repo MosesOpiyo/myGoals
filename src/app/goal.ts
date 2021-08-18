4ebda135663448a5fb9f37a39f0e5dc10b2c001e
Moses <moses.junior@student.moringaschool.com>
2021-08-18 14:01:38 +0300

    changes

--- src/app/goal.ts
+++ src/app/goal.ts
@@ -1,4 +1,5 @@
 export class Goal {
 id:number;
-name: string
+name: string;
+description: string;
 }
--- src/app/goal/goal.component.ts
+++ src/app/goal/goal.component.ts
@@ -9,9 +9,9 @@ import { Goal } from '../goal';
 export class GoalComponent implements OnInit {
   title = 'My Goals'
    goals:Goal[]=[
-    {id:1,name:'watch tv'},
-    {id:2,name:'buy a new phone'},
-    {id:3,name:'get a new watch'}
+    {id:1,name:'watch tv',description:'lots and lots of TV'},
+    {id:2,name:'buy a new phone',description:'Dont know wich one though'},
+    {id:3,name:'get a new watch',description:'options of a rolex or patek'}
    ]
 
   constructor() { }
